6e527c71e1a5ace836199b01f801ec0114aa7d1a
NotNorman <Chief4killin@hotmail.com>
2026-07-02 14:07:23 -0400

    commit riot_apy.py

--- riot_api.py
+++ riot_api.py
@@ -1,12 +1,9 @@
 import os
 from dotenv import load_dotenv
-
 import requests
-
 load_dotenv()
 
 API_KEY = os.getenv("RIOT_API_KEY")
-
 request_url = "https://americas.api.riotgames.com/riot/account/v1/"
 
 def get_account_info_by_riot_id(gameName, tagLine):
@@ -36,19 +33,10 @@ def get_account_info_by_puuid(puuid):
         print(f"Failed to retrieve data {response.status_code}")
 
 
-gameName = 'Zeeshan Ahmad'
-tagLine = "bkini"
 
-account_info = get_account_info_by_riot_id(gameName, tagLine)
+account_info = get_account_info_by_puuid("7lWXd76TB3lwlSy76XGwUj-fDBoJPqLHryvlq3JVIOk-kbhknMf5RaK57AEF5OPIYd6X2G6i8p9dyA")
 
 if account_info:
-    print(f"{account_info["puuid"]}")
-    print(f"{account_info["gameName"]}")
-    print(f"{account_info["tagLine"]}")
-
-account_info2 = get_account_info_by_puuid("7lWXd76TB3lwlSy76XGwUj-fDBoJPqLHryvlq3JVIOk-kbhknMf5RaK57AEF5OPIYd6X2G6i8p9dyA")
-
-if account_info2:
-    print(f"{account_info2["puuid"]}")
-    print(f"{account_info2["gameName"]}")
-    print(f"{account_info2["tagLine"]}")
+    print(f"{account_info['puuid']}")
+    print(f"{account_info['gameName']}")
+    print(f"{account_info['tagLine']}")

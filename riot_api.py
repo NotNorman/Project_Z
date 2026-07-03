b2fe57385b07f711f79409cd5978386fc6af9b30
NotNorman <Chief4killin@hotmail.com>
2026-07-03 15:38:06 -0400

    created search matches by puuid

--- riot_api.py
+++ riot_api.py
@@ -4,10 +4,11 @@ import requests
 load_dotenv()
 
 API_KEY = os.getenv("RIOT_API_KEY")
-request_url = "https://americas.api.riotgames.com/riot/account/v1/"
+account_v1_url = "https://americas.api.riotgames.com/riot/account/v1/"
+match_v5_url = "https://americas.api.riotgames.com/lol/match/v5/matches/"
 
 def get_account_info_by_riot_id(gameName, tagLine):
-    api_path = f"{request_url}accounts/by-riot-id/"
+    api_path = f"{account_v1_url}accounts/by-riot-id/"
 
     url = f"{api_path}{gameName}/{tagLine}?api_key={API_KEY}"
     response = requests.get(url)
@@ -20,7 +21,7 @@ def get_account_info_by_riot_id(gameName, tagLine):
         print(f"Failed to retrieve data {response.status_code}")
 
 def get_account_info_by_puuid(puuid):
-    api_path = f"{request_url}accounts/by-puuid/"
+    api_path = f"{account_v1_url}accounts/by-puuid/"
 
     url = f"{api_path}{puuid}/?api_key={API_KEY}"
     response = requests.get(url)
@@ -32,7 +33,18 @@ def get_account_info_by_puuid(puuid):
     else:
         print(f"Failed to retrieve data {response.status_code}")
 
+def get_recent_matches_by_puuid(puuid):
+    api_path = f"{match_v5_url}by-puuid/"
 
+    url = f"{api_path}{puuid}/ids?start=0&count=20&api_key={API_KEY}"
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code == 200:
+        match_ids = response.json()
+        return(match_ids)
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
 
 account_info = get_account_info_by_puuid("7lWXd76TB3lwlSy76XGwUj-fDBoJPqLHryvlq3JVIOk-kbhknMf5RaK57AEF5OPIYd6X2G6i8p9dyA")
 
@@ -40,3 +52,5 @@ if account_info:
     print(f"{account_info['puuid']}")
     print(f"{account_info['gameName']}")
     print(f"{account_info['tagLine']}")
+
+print(get_recent_matches_by_puuid("7lWXd76TB3lwlSy76XGwUj-fDBoJPqLHryvlq3JVIOk-kbhknMf5RaK57AEF5OPIYd6X2G6i8p9dyA"))
